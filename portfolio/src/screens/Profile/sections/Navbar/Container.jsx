4e8b83412352eb17c8e17d71a4a3f2b43e85d4c2
Barry Lin <barrylin6723@gmail.com>
2025-11-15 16:05:08 -0500

    Revert "adding mobile support"
    This reverts commit 1be416758763b786d9019a8b33174b2430624428.

--- portfolio/src/screens/Profile/sections/Navbar/Container.jsx
+++ portfolio/src/screens/Profile/sections/Navbar/Container.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 import { Component } from "../../../../components/Component";
 import "./style.css";
 
@@ -14,17 +14,10 @@ const navItems = [
   { text: "Projects", id: "projects" },
   { text: "Education", id: "Education" },
   { text: "Get in touch", id: "lets get to know" },
-
+  
 ];
 
 export const Container = () => {
-  const [mobileMenuOpen, setMobileMenuOpen] = useState(false);
-
-  const handleNavClick = (id) => {
-    scrollToSection(id);
-    setMobileMenuOpen(false); // Close menu after clicking
-  };
-
   return (
     <div className="container">
       <div className="nav">
@@ -36,18 +29,7 @@ export const Container = () => {
           </div>
         </div>
 
-        {/* Hamburger Menu Button */}
-        <div
-          className="hamburger-menu"
-          onClick={() => setMobileMenuOpen(!mobileMenuOpen)}
-          aria-label="Toggle menu"
-        >
-          <span></span>
-          <span></span>
-          <span></span>
-        </div>
-
-        <div className={`div-3 ${mobileMenuOpen ? 'mobile-open' : ''}`}>
+        <div className="div-3">
           {navItems.map(({ text, id }) => (
             <div className="container-wrapper-3" key={id}>
               <div className="component-wrapper">
@@ -56,7 +38,7 @@ export const Container = () => {
                   text={text}
                   textClassName="component-instance"
                   variant="two"
-                  onClick={() => handleNavClick(id)}
+                  onClick={() => scrollToSection(id)}
                 />
               </div>
             </div>
